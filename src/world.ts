628b9a37f3b5cb038b2b7cd5cfb6ac63f458e2f6
SID37 <1999login@mail.ru>
2019-08-05 22:47:25 +0300

    работает очередь

--- src/world.ts
+++ src/world.ts
@@ -60,15 +60,20 @@ class Entity extends Typeable implements Drawable{
 
     private drawHitbox = (hitbox: Hitbox) => {}
     velocity: Point;
+    collision: CollisionPair;
+    tag: number;
 
     tick(dt: number){
         this.hitbox.position.x += dt * this.velocity.x;
         this.hitbox.position.y += dt * this.velocity.y;
+        this.collision.time -= dt;
     }
 
     constructor(public hitbox: Hitbox, public avatar: Avatar) {
         super("Entity");
-        this.velocity = new Point({x:1, y:2});
+        this.velocity = new Point({x:Math.random() * 5, y:Math.random() * 5});
+        this.collision = new CollisionPair(this);
+        this.tag = 0;
     }
 
     Acceleration():Point{
@@ -81,12 +86,14 @@ class Entity extends Typeable implements Drawable{
 }
 
 class World extends Typeable implements Drawable{
+
     setContext(context: CanvasRenderingContext2D): Drawable {
         this.context = context;
         this.mobs.forEach(mob => mob.setContext(context))
         this.user.setContext(context)
         return this;
     }
+
     draw(): void {
         this.context.fillStyle = "#000"
         this.context.fillRect(0, 0, 512, 512);
@@ -98,35 +105,93 @@ class World extends Typeable implements Drawable{
         });
         this.user.draw();
     }
+
     user: Entity;
     mobs: Array<Entity>;
     private context!: CanvasRenderingContext2D;
+    private queue: PriorityQueue;
 
     constructor(user: Entity) {
         super("World");
         this.user = user;
         this.mobs = new Array<Entity>();
+        this.queue = new PriorityQueue();
+    }
+
+    Contact(pair: CollisionPair){
+        let buf = pair.e1.velocity;
+        pair.e1.velocity = pair.e2.velocity;
+        pair.e2.velocity = buf;
     }
 
     tick(dt: number){
-        for(let i = 0; i < this.mobs.length; ++i)
-            if(i % 5 == 3)    
-                this.mobs[i].velocity.x = 3;
-        //console.log(this.mobs[2].hitbox.x1 - this.mobs[3].hitbox.x1, this.mobs[2].hitbox.y1 - this.mobs[3].hitbox.y1);
-        //console.log(Collision.EntityesTime(this.mobs[2], this.mobs[3]));
-        Collision.EntityesTime(this.mobs[2], this.mobs[3]);
-        this.mobs.forEach(function(mob) {
-            mob.tick(dt);
-        })
+        do
+        {
+            let pair = this.queue.Better().collision;
+
+            let contact = pair.time <= dt;
+
+            if (!contact){
+                this.mobs.forEach(mob => mob.tick(dt))
+                dt = -1;
+                continue;
+            }
+
+            dt -= pair.time;
+            let delta = pair.time - 1e-9;
+            this.mobs.forEach(mob => mob.tick(delta))
+
+            this.Contact(pair);
+
+            this.Update(pair.e1);
+            this.Update(pair.e2);
+        } while (dt > 0);
+
+        console.log(this.queue.list.length, this.queue.Better().collision.time);
+
     }
 
+    add(entity: Entity){
+        this.mobs.push(entity);
+        this.queue.Add(entity);
+        this.Update(entity);
+    }
+
+    Update(entity: Entity)
+    {
+        let time = Infinity; 
+        let other = entity;
+        for (let i = 0; i < this.mobs.length; ++i){
+            let current = this.mobs[i];
+            if (current != entity)
+            {
+                let currentTime = Collision.EntityesTime(entity, current, time);
+                if (currentTime < time && currentTime < current.collision.time)
+                {
+                    other = current;
+                    time = currentTime;
+                }
+            }
+        }
+
+        let pair1 = new CollisionPair(entity, other, time, new Point({}));
+        entity.collision = pair1;
+        this.queue.Relocate(entity.tag);
+        if(entity != other)
+        {
+            let pair2 = new CollisionPair(entity, other, time, new Point({}));
+            other.collision = pair2;
+            this.queue.Relocate(other.tag);
+        }
+    }
     pushDrawable(entity: Entity) {
-        this.mobs.push(entity.setContext(this.context) as Entity);
+        this.add(entity.setContext(this.context) as Entity);
     }
 
     static unpack({user, mobs}: {user: Entity, mobs: Array<Entity>}) {
-        let w = new World(user);
-        w.mobs = mobs;
+        let w = new World(user);                
+        mobs.forEach(mob => console.log(mob.collision.time));
+        mobs.forEach(mob => w.add(mob));
         return w;
     }
 }
--- static/js/world.js
+++ static/js/world.js
@@ -41,7 +41,9 @@ class Entity extends Typeable {
         this.hitbox = hitbox;
         this.avatar = avatar;
         this.drawHitbox = (hitbox) => { };
-        this.velocity = new Point({ x: 1, y: 2 });
+        this.velocity = new Point({ x: Math.random() * 5, y: Math.random() * 5 });
+        this.collision = new CollisionPair(this);
+        this.tag = 0;
     }
     setContext(context) {
         this.drawHitbox = this.avatar.bindContext(context);
@@ -53,6 +55,7 @@ class Entity extends Typeable {
     tick(dt) {
         this.hitbox.position.x += dt * this.velocity.x;
         this.hitbox.position.y += dt * this.velocity.y;
+        this.collision.time -= dt;
     }
     Acceleration() {
         return new Point({});
@@ -83,24 +86,65 @@ class World extends Typeable {
         super("World");
         this.user = user;
         this.mobs = new Array();
+        this.queue = new PriorityQueue();
+    }
+    Contact(pair) {
+        let buf = pair.e1.velocity;
+        pair.e1.velocity = pair.e2.velocity;
+        pair.e2.velocity = buf;
     }
     tick(dt) {
-        for (let i = 0; i < this.mobs.length; ++i)
-            if (i % 5 == 3)
-                this.mobs[i].velocity.x = 3;
-        //console.log(this.mobs[2].hitbox.x1 - this.mobs[3].hitbox.x1, this.mobs[2].hitbox.y1 - this.mobs[3].hitbox.y1);
-        //console.log(Collision.EntityesTime(this.mobs[2], this.mobs[3]));
-        Collision.EntityesTime(this.mobs[2], this.mobs[3]);
-        this.mobs.forEach(function (mob) {
-            mob.tick(dt);
-        });
+        do {
+            let pair = this.queue.Better().collision;
+            let contact = pair.time <= dt;
+            if (!contact) {
+                this.mobs.forEach(mob => mob.tick(dt));
+                dt = -1;
+                continue;
+            }
+            dt -= pair.time;
+            let delta = pair.time - 1e-9;
+            this.mobs.forEach(mob => mob.tick(delta));
+            this.Contact(pair);
+            this.Update(pair.e1);
+            this.Update(pair.e2);
+        } while (dt > 0);
+        console.log(this.queue.list.length, this.queue.Better().collision.time);
+    }
+    add(entity) {
+        this.mobs.push(entity);
+        this.queue.Add(entity);
+        this.Update(entity);
+    }
+    Update(entity) {
+        let time = Infinity;
+        let other = entity;
+        for (let i = 0; i < this.mobs.length; ++i) {
+            let current = this.mobs[i];
+            if (current != entity) {
+                let currentTime = Collision.EntityesTime(entity, current, time);
+                if (currentTime < time && currentTime < current.collision.time) {
+                    other = current;
+                    time = currentTime;
+                }
+            }
+        }
+        let pair1 = new CollisionPair(entity, other, time, new Point({}));
+        entity.collision = pair1;
+        this.queue.Relocate(entity.tag);
+        if (entity != other) {
+            let pair2 = new CollisionPair(entity, other, time, new Point({}));
+            other.collision = pair2;
+            this.queue.Relocate(other.tag);
+        }
     }
     pushDrawable(entity) {
-        this.mobs.push(entity.setContext(this.context));
+        this.add(entity.setContext(this.context));
     }
     static unpack({ user, mobs }) {
         let w = new World(user);
-        w.mobs = mobs;
+        mobs.forEach(mob => console.log(mob.collision.time));
+        mobs.forEach(mob => w.add(mob));
         return w;
     }
 }
